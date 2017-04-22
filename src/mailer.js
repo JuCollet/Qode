2abe5ef33fb8bdbbc8f599e9c615cf802348309a
ju.collet <ju.collet@gmail.com>
2017-04-22 05:46:31 -0400

    Add mailer

---
+++ src/mailer.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
+const welcomeMail = function(username, usermail){
+  const request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              email: usermail,
+            },
+          ],
+          subject: 'Welcome to Qode, '+username,
+        },
+      ],
+      from: {
+        email: 'noreply@qode.be',
+      },
+      content: [
+        {
+          type: 'text/plain',
+          value: "Hello "+username+" !",
+        },
+      ],
+    },
+  });
+  
+sg.API(request, function(error, response) {
+  if (error) {
+    console.log('Error response received');
+  }
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+});
+  
+};
+
+module.exports = welcomeMail;
