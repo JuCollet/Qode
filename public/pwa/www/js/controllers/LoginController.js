2d4d16b7eb57ab45a59c3f0e0688890c667aa84e
Julien <ju.collet@gmail.com>
2017-05-03 09:32:06 +0000

    Adding loading wheel

--- public/pwa/www/js/controllers/LoginController.js
+++ public/pwa/www/js/controllers/LoginController.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('mobile')
-.controller('LoginController', ['$scope', '$rootScope', '$state', '$ionicPopup', '$timeout', 'userFactory', '$ionicHistory', function($scope, $rootScope, $state, $ionicPopup, $timeout, userFactory, $ionicHistory){
-    
+.controller('LoginController', ['$scope', '$rootScope', '$state', '$ionicPopup', '$timeout', 'userFactory', '$ionicHistory', '$ionicLoading', function($scope, $rootScope, $state, $ionicPopup, $timeout, userFactory, $ionicHistory, $ionicLoading){
     
     // Didn't used ng-model for form inputs because it didn't updated values with mobile form auto-completion (ios)
     // So when user/password where auto-filled, the ng-models where empty when the doLogin function was triggered.
     $scope.doLogin = function(){
+        $ionicLoading.show();
         const user = {
             mail: document.login.mail.value,
             password: document.login.password.value
@@ -22,7 +22,9 @@ angular.module('mobile')
                 disableBack: true
             });
             $state.go('app.search');
+            $ionicLoading.hide();
         },function(){
+            $ionicLoading.hide();
             const alertPopup = $ionicPopup.alert({
              title: 'Oops..',
              template: "We didn't find you"
